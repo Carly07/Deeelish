84af599d6256ae7555fa238c063a2fb5f808eefe
Carly07 <carlysimpson07@hotmail.co.uk>
2020-03-08 12:25:45 +0000

    Created an Add Recipe form and associated functionality

--- app.py
+++ app.py
@@ -11,10 +11,25 @@ app.config["MONGO_URI"] = 'mongodb+srv://Carly0707:Torrevieja53@myfirstcluster-d
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
+
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html',
+            meals_courses=mongo.db.meals_courses.find(),
+            cuisines=mongo.db.cuisines.find())
+
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
 
 
 if __name__ == '__main__':
